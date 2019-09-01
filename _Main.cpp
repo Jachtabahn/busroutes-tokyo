c65083fdfb877bf4b03ac42497c84e3b5b012bdd
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2019-09-01 21:22:39 +0200

    Make solution variables local

--- _Main.cpp
+++ _Main.cpp
@@ -18,61 +18,55 @@ double since(clock_t start)
 
 #include "parse.hpp"
 
-namespace solution
+int main()
 {
+    clock_t total_start = clock();
+
+    std::clog.precision(std::numeric_limits<double>::max_digits10);
+
+    // solution variables
     std::vector<std::unique_ptr<intersection::Region>> regions;
     std::vector<std::unique_ptr<intersection::Route>> routes;
     double budget;
-
-    intersection::Box routesBox {intersection::supremum, intersection::infimum};
     double costGcd;
     double minCost {std::numeric_limits<double>::infinity()};
+    std::map<int, int> allocation;
 
-    std::map<std::string, int> allocation;
-}
-
-int main()
-{
-    std::clog.precision(std::numeric_limits<double>::max_digits10);
-
-    clock_t total_start = clock();
     clock_t start = clock();
-    parse::input(solution::regions, solution::routes, solution::budget, solution::minCost, solution::costGcd, solution::routesBox);
+    parse::input(regions, routes, budget, minCost, costGcd);
     std::clog << "Parsing of all input took " << since(start) << "ms" << std::endl;
-    // std::clog << "The box enclosing all routes is " << solution::routesBox << std::endl;
-    // std::clog << "My total budget is " << solution::budget << std::endl;
-    // std::clog << "The minimum cost is " << solution::minCost << std::endl;
-    // std::clog << "The greatest common divisor of all costs is " << solution::costGcd << std::endl;
+    std::clog << "The minimum cost is " << minCost << std::endl;
+    std::clog << "The greatest common divisor of all costs is " << costGcd << std::endl;
 
     start = clock();
-    intersection::all(solution::regions, solution::routes);
+    intersection::all(regions, routes);
     std::clog << "Evaluating all routes took " << since(start) << "ms" << std::endl;
 
     start = clock();
-    knapsack::values(solution::routes, solution::budget, solution::minCost, solution::costGcd, solution::allocation);
+    knapsack::values(routes, budget, minCost, costGcd, allocation);
     std::clog << "Optimizing my allocation took " << since(start) << "ms" << std::endl;
 
     /*
     std::clog << "\n" << std::endl;
     for (size_t i = 0; i < 6; ++i)
     {
-        auto& region = *solution::regions[i];
+        auto& region = *regions[i];
         std::clog << region;
     }
     std::clog << "\n" << std::endl;
     for (size_t i = 0, size = 6; i < size; ++i)
     {
-        auto& route = *solution::routes[i];
+        auto& route = *routes[i];
         std::clog << route;
     }
     */
 
     // Output our solution
     // std::clog << "\nMy allocation is" << std::endl;
-    for (const auto& iter : solution::allocation)
+    for (const auto& iter : allocation)
     {
         std::cout << iter.first << "," << iter.second << "\n";
-        // std::clog << iter.first << "," << iter.second << std::endl;
+        std::clog << iter.first << "," << iter.second << std::endl;
     }
 
     std::clog << "----------------------------------\n";
