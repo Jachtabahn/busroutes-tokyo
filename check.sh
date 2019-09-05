72396d2dadaa298e50084372771fd26be8fb600f
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2019-09-05 02:20:07 +0200

    Remove checker source and scripts

--- check.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./checker ./data/Route.geojson ./data/Population_1.geojson ./data/active.csv example.out "1,2,5" 10000000 5000
--- checkbuild.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ checker.cpp -o checker -O2 -std=c++11 -Wno-format
